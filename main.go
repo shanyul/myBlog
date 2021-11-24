2b8cf3965f78c56545f397b916d97853ad9eb9d1
liangqingyu <liangqingyu@huishoubao.com.cn>
2021-11-24 21:05:35 +0800

    update

--- main.go
+++ main.go
@@ -5,11 +5,28 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprint(w, "<h1>Hello, 这里是 goblog</h1>")
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Hello, 欢迎来到 goblog！</h1>")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>")
+	}
+}
+
+func aboutHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprint(w, "<h1>关于我们</h1>")
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":8080", nil)
+	// 重构 HandleFunc 的底层 ServeMux 的实现
+	router := http.NewServeMux()
+
+	router.HandleFunc("/", defaultHandler)
+	router.HandleFunc("/about", aboutHandler)
+
+	// http.HandleFunc("/", defaultHandler)
+	// http.HandleFunc("/about", aboutHandler)
+	http.ListenAndServe(":8080", router)
 }
